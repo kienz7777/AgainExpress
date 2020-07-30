4e9510c5e154ae9d6e2d1146555a38b133e777af
kienz7777 <kienvu729>
2020-07-30 09:29:12 +0700

    cart user

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -20,5 +20,19 @@ module.exports.addToCart = (req,res,next) => {
       .set('cart.' + productId, count + 1)
       .write();
 
+    if(req.signedCookies.userId){
+
+        var countC = db
+            .get('sessions')
+            .find({userId: req.signedCookies.userId})
+            .get('cart.' + productId, 0)
+            .value();
+
+        db.get('sessions')
+          .find({userId: req.signedCookies.userId})
+          .set('cart.' + productId, countC + 1)
+          .write();
+    }
+
     res.redirect('/products');
 };
--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -15,9 +15,54 @@ module.exports = (req,res,next) => {
  
     if(req.signedCookies.userId){
         var x = db.get('users').find({id : req.signedCookies.userId}).value()
-        console.log(x.name);
+        //console.log(x.name);
         res.locals.userName = x.name; 
+
+        // ko có userId thì mới được add userId vô session
+        var userCart = db.get('sessions').find({userId : req.signedCookies.userId }).value()
+
+        if(!userCart){
+            //db.get('sessions').remove({userId : req.signedCookies.userId }).write()
+            db.get('sessions')
+              .find({id: req.signedCookies.sessionId})
+              .set('userId' , req.signedCookies.userId)
+              .write();
+        }
+
+        //show numberCart của user
+        var countU = 0
+
+        if(userCart.cart){
+            for (let [key, value] of Object.entries(userCart.cart)) {
+            
+                countU = countU + value;
+                
+            }
+        }
+        console.log(countU);
+        res.locals.numberCart = countU;
     }   
-    
+
+    if(req.signedCookies.sessionId){
+        var count = 0
+
+        var cart = db.get('sessions').find({id : req.signedCookies.sessionId}).value()
+        //console.log(cart.cart);
+
+        if(cart.cart){
+            for (let [key, value] of Object.entries(cart.cart)) {
+                //console.log(key + ': ' + value);
+                count = count + value;
+                
+            }
+        }
+
+        //console.log(count);      show numberCart của guess
+        if(!req.signedCookies.userId){
+            res.locals.numberCart = count;
+        }
+               
+    }
+
     next();
 };
