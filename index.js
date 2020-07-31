859fc33e8bf065990ac40af338c22897cff7d843
kienz7777 <kienvu729>
2020-07-31 10:08:12 +0700

    add transfer csrf

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+
 var userRoute = require('./routers/user.route');
 var authRoute = require('./routers/auth.route');
 var authMiddleware = require('./middleware/auth.middleware');
 var productRoute = require('./routers/product.route');
 var sessionMiddleware = require('./middleware/session.middleware');
 var cartRoute = require('./routers/cart.route');
+var transferRoute = require('./routers/transfer.route');
+
 var port = 3000;
 
 var app = express();
@@ -20,8 +23,8 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SECRET_Cookie));
 app.use(express.static('public'));
-
 app.use(sessionMiddleware);
+
 
 app.get('/', (req, res) => { res.render('index', { name: 'kienz' }) });
 
@@ -29,5 +32,6 @@ app.use('/users',authMiddleware.requireAuth,userRoute);
 app.use('/auth',authRoute);
 app.use('/products',productRoute);
 app.use('/cart', cartRoute);
+app.use('/transfer', authMiddleware.requireAuth, transferRoute);
 
 app.listen(port, () => { console.log('Server listening on port ' + port); })
